c1c4712476757ffbe4fa819f4929791f479d3a6b
taucody <nia.deux.zero@gmail.com>
2026-03-31 11:25:30 -0400

    modification des couleurs et ajout du fichier NetworkScanningScreen

---
+++ app/src/main/java/com/example/neteyeon/screens/NetworkScanningScreen.kt
@@ -0,0 +1,196 @@
+package com.example.neteyeon.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.composables.icons.lucide.Locate
+import com.composables.icons.lucide.Lucide
+import com.composables.icons.lucide.Radar
+import com.example.neteyeon.screens.CGUScreen
+import com.example.neteyeon.components.AcceptCguCheckbox
+import com.example.neteyeon.ui.theme.NetEyeOnTheme
+
+@Composable
+fun NetworkScanningScreen(
+    ipRange: String,
+    onScanClicked: (scanType: String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var scanType by remember { mutableStateOf("") }
+
+    val scanDescription = when (scanType) {
+        "Basique" -> "Scan simple et rapide des hôtes disponibles."
+        "Avance" -> "Scan optimisé pour obtenir des résultats en peu de temps."
+        "Custom" -> "Scan plus complet avec davantage de vérifications réseau."
+        else -> "Sélectionnez un profil de scan."
+    }
+
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Icon(
+                imageVector = Lucide.Radar,
+                contentDescription = "Radar",
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding( 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Text("Prêt à Scanner le réseau suivant: ", style = MaterialTheme.typography.titleLarge)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Surface(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .heightIn(min = 80.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Text("Plage à scanner", style = MaterialTheme.typography.bodySmall)
+
+                    Text(text = ipRange, style = MaterialTheme.typography.bodyLarge)
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Text(
+                text = "Profil de scan",
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.fillMaxWidth(0.85f),
+                textAlign = TextAlign.Start
+            )
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(0.85f),
+                thickness = 3.dp,
+                color = MaterialTheme.colorScheme.outline
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Surface(
+                color = MaterialTheme.colorScheme.onSecondary,
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        modifier = Modifier.padding(8.dp),
+                        onClick = { scanType = "Basique" },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = if (scanType == "Basique")
+                                MaterialTheme.colorScheme.primary
+                            else
+                                MaterialTheme.colorScheme.surfaceVariant,
+                            contentColor = if (scanType == "Basique")
+                                MaterialTheme.colorScheme.onPrimary
+                            else
+                                MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    ) {
+                        Text("Basique")
+                    }
+
+                    Button(
+                        modifier = Modifier.padding(8.dp),
+                        onClick = { scanType = "Avance" },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = if (scanType == "Avance")
+                                MaterialTheme.colorScheme.primary
+                            else
+                                MaterialTheme.colorScheme.surfaceVariant,
+                            contentColor = if (scanType == "Avance")
+                                MaterialTheme.colorScheme.onPrimary
+                            else
+                                MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    ) {
+                        Text("Avance")
+                    }
+
+                    Button(
+                        modifier = Modifier.padding(8.dp),
+                        onClick = { scanType = "Custom" },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = if (scanType == "Custom")
+                                MaterialTheme.colorScheme.primary
+                            else
+                                MaterialTheme.colorScheme.surfaceVariant,
+                            contentColor = if (scanType == "Custom")
+                                MaterialTheme.colorScheme.onPrimary
+                            else
+                                MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    ) {
+                        Text("Custom")
+                    }
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Surface(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.fillMaxWidth(0.85f)
+
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Text("Description du profil de scan")
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = scanDescription,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
+
+
+            }
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Button(onClick = { onScanClicked(scanType) }) {
+                Text("Scanner", style = MaterialTheme.typography.titleMedium)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNetworkScanningScreen() {
+    NetEyeOnTheme {
+        NetworkScanningScreen(ipRange = "123.432.132/24", onScanClicked = {})
+    }
+}
--- app/src/main/java/com/example/neteyeon/ui/theme/Theme.kt
+++ app/src/main/java/com/example/neteyeon/ui/theme/Theme.kt
@@ -1,54 +1,57 @@
 package com.example.neteyeon.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkBluePrimary,
+    secondary = DarkBlueSecondary,
+    tertiary = DarkBlueTertiary,
+
+    background = DarkBackground,
+    surface = DarkSurface,
+    surfaceVariant = DarkSurfaceVariant,
+
+    onPrimary = DarkBackground,
+    onSecondary = DarkBackground,
+    onTertiary = DarkBackground,
+
+    onBackground = DarkTextPrimary,
+    onSurface = DarkTextPrimary,
+    onSurfaceVariant = DarkTextSecondary,
+
+    outline = DarkOutline
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BluePrimary,
+    secondary = BlueSecondary,
+    tertiary = BlueTertiary,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = BeigeBackground,
+    surface = BeigeSurface,
+    surfaceVariant = BeigeSurfaceVariant,
+
+    onPrimary = WhiteSoft,
+    onSecondary = TextPrimary,
+    onTertiary = TextPrimary,
+
+    onBackground = TextPrimary,
+    onSurface = TextPrimary,
+    onSurfaceVariant = TextSecondary,
+
+    outline = OutlineSoft
 )
 
 @Composable
 fun NetEyeOnTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
